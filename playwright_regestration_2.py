299bf39a16a682dd821eed6ab311a366d96b4273
grdebrin <gr.debrin@gmail.com>
2026-02-23 12:59:00 +0300

    Работа с авторизацией в Playwright

---
+++ playwright_regestration_2.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+
+    email_input.fill('user@gmail.com')
+    username_input.fill('username')
+    password_input.fill('password')
+    registration_button.click()
+
+    page.wait_for_load_state("networkidle")
+
+    context.storage_state(path='browser-state.json')
+
+    page.wait_for_timeout(5000)
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+
+    page.wait_for_timeout(5000)
+
+
+
+
+
+
+
+
