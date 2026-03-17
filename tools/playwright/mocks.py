e256cc9aca3095d7918f9201268915b1c630e09d
grdebrin <gr.debrin@gmail.com>
2026-03-17 18:13:26 +0300

    Логирование взаимодействий со страницей

---
+++ tools/playwright/mocks.py
@@ -0,0 +1,6 @@
+from playwright.sync_api import Page, Route
+
+
+def mock_static_resources(page: Page):
+    page.route("**/*.{ico,png,jpg,svg,webp,mp3,mp3,woff,woff2}", lambda route: route.abort())
+
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,6 +1,8 @@
 import allure
 from playwright.sync_api import Page, Playwright
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
         playwright: Playwright,
@@ -12,6 +14,7 @@ def initialize_playwright_page(
     context = browser.new_context(base_url=settings.get_base_url(), storage_state=storage_state, record_video_dir=settings.videos_dir)
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)
     yield page
     context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
     context.close()
