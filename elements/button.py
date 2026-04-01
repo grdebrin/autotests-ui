9f804e187f040fc238fea7c52b0909f0639ddf25
grdebrin <gr.debrin@gmail.com>
2026-04-01 11:32:51 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/button.py
+++ elements/button.py
@@ -1,6 +1,8 @@
+from ui_coverage_tool import ActionType
+
 from elements.base_element import BaseElement
 import allure
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Locator
 
 from tools.logger import get_logger
 
@@ -11,6 +13,12 @@ class Button(BaseElement):
     def type_of(self) -> str:
         return "button"
 
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
+        return super().get_locator(nth, **kwargs).locator('button')
+
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(nth, **kwargs)}//button'
+
     def check_enabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is enabled'
 
@@ -19,6 +27,8 @@ class Button(BaseElement):
             logger.info(step)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
 
@@ -26,5 +36,7 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
 
     
